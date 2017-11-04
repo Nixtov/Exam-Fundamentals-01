18de9c231e7cebbfc9f1797590a6563255c7c769
Nixtov <nixtov@gmail.com>
2017-11-04 20:37:35 +0200

    Add project files.

---
+++ CoffeeOrders/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CoffeeOrders
+{
+   public static void Main()
+    {
+        int countN = int.Parse(Console.ReadLine());
+        decimal totalPrice = 0;
+        for (int i = 0; i < countN; i++)
+        {
+           decimal price = ProgressOrder();
+            totalPrice += price;
+        }
+        Console.WriteLine($"Total: ${totalPrice:F2}");
+    }
+
+     static decimal ProgressOrder()
+    {
+        throw new NotImplementedException();
+    }
+}
+
